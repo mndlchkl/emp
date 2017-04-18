1bd51c84ce7341aa2ff311eb313733fda696ef54
FranciscoRuiz <franruiz21@gmail.com>
2017-04-18 15:04:47 -0300

    terminos de postulacion, listado postulantes

--- 3/endpoints/postular.php
+++ 3/endpoints/postular.php
@@ -1,8 +1,4 @@
 <?php
-
- # id, name, last_name, email, phone, rut, project_name, experience, motivations, music_work_url
- 
-
 $name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
 $phone = $_POST['phone'];
@@ -15,19 +11,17 @@ $music_work_url = $_POST['music_link'];
  
 $servername = "localhost";
 $username = "root";
-$password = "eb436ec1de19aef9d339deffa9aa2013804a7e225a7ccc21";
+$password = "godzuki";
 $dbname = "ecpm";
  
-
 $mysqli = new mysqli($servername,$username,$password,$dbname);
 mysqli_set_charset($mysqli,"utf8");
- 
+
 ////// REVISAR SI EL RUT YA EXISTE
 $checkrut=("select * from aplicant where rut =  '".$rut."' ");
  if($result1 = $mysqli->query($checkrut)){ 
-  		 $guardar = mysql_num_rows ($result1); 
+  		 $guardar =  $row_cnt = $result1->num_rows;// mysql_num_rows($result1); 
  }
-
 //////////////////AHORA SI GUARDAR
 if ($guardar==0){
 	$query=("insert into aplicant(name, last_name, email, phone, rut, project_name, experience, motivations, music_work_url) 
@@ -36,16 +30,8 @@ if ($guardar==0){
 	 if($result = $mysqli->query($query)){ 
   		  echo   "Postulación recibida."; 
 	 }else echo $mysqli->errno;
-
-	 
-	 
-	       
-	    echo   "Postulación recibida.";   
-	}
-
  }else echo "El rut ya está registrado.";
-
- mysql_close($link);
+ mysqli_close($mysqli);
 ?>
 
 
