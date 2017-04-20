e8ee8da007fecac5ee61ff16cf324060ab61a33b
laloinsane <calisto.michel@gmail.com>
2017-04-19 22:55:47 -0300

    ahora si, con load incluido

--- 3/js/insta-final.js
+++ 3/js/insta-final.js
@@ -2,7 +2,7 @@ var token = '4903487647.4eec902.f2d19fbaa8084c50883f4480a8b70dae',
     username = 'ecpm_sur',
     num_photos = 9;
 
-//$('#insta-grid').html('<div class="center-align" style="padding-top: 100px"><img src="images/gears.svg"></div>');
+$('#insta-grid').html('<div id="load" class="center-align" style="padding-top: 100px"><img src="images/gears.svg"></div>');
 
 $.ajax({
 	url: 'https://api.instagram.com/v1/users/search',
@@ -18,6 +18,7 @@ $.ajax({
 			data: {access_token: token, count: num_photos},
 			success: function(data2){
 				console.log(data2);
+				$("#load").remove();
 				for(x in data2.data){
 					if (data2.data[x].type == "image"){
 						$('#insta-grid').append('<li><img src="'+data2.data[x].images.standard_resolution.url+'"><a target="_blank" href="'+data2.data[x].link+'" class="instalink"><div class="caption right-align"><h3>'+data2.data[x].caption.text+'</h3><h4 class="light grey-text text-lighten-3"><i class="fa fa-heart"></i>&nbsp;'+data2.data[x].likes.count+'&emsp;<i class="fa fa-comment"></i>&nbsp;'+data2.data[x].comments.count+'</h4></div></a></li>');
